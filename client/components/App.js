aed48143b413717f07275e1e49dfb61e5dc1dd2a
Brooks Becton <broabect@ut.utm.edu>
2017-03-25 13:51:31 -0500

    Seperated firebase meta info into env file

--- client/components/App.js
+++ client/components/App.js
@@ -5,16 +5,9 @@ import {
   Link
 } from 'react-router-dom'
 import * as firebase from "firebase";
+const env = require('./../env.js')
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCU9lLaUVE7v-hiDdQVjGjvRStdw-6XH50",
-  authDomain: "evelyn-f9eb7.firebaseapp.com",
-  databaseURL: "https://evelyn-f9eb7.firebaseio.com",
-  storageBucket: "evelyn-f9eb7.appspot.com",
-  messagingSenderId: "255158556452"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(env.config);
 const provider = new firebase.auth.GoogleAuthProvider();
 
 import Search from './Search'
@@ -34,20 +27,14 @@ export default class App extends React.Component {
     const _this = this;
 
     firebase.auth().signInWithPopup(provider).then(function (result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
       _this.setState({ user: user });
     }).catch(function (error) {
-      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's account used.
       var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
       var credential = error.credential;
-      // ...
       console.log(errorMessage);
     });
 
